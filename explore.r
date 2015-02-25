23e23dd147d498e42c281703380e2d7e2809ebd8
Tim Hoolihan <tim@hoolihan.net>
2015-02-25 16:30:32 -0500

    Fixed lines and points

--- explore.r
+++ explore.r
@@ -7,16 +7,21 @@ with(data, {
      plot(Day.Index, 
           Pageviews,
           xlab = "Date",
-          type = "b",
-          pch = 21,
-          bg = "navy",
-          col = "navy",
+          type = "l",
+          col = "white",
           main = "Google Analytics",
           ylim = c(0, 200)) 
      
      xarea <- c(Day.Index, rev(Day.Index))
      yarea <- c(Pageviews, rep(0, nrow(data)))
-     polygon(xarea, yarea, col = rgb(0.3, 0.8, 1, 0.5), border=NA)     
+     polygon(xarea, yarea, col = rgb(0.3, 0.8, 1, 0.5), border=NA)      
+     
+     lines(Pageviews ~ Day.Index, col = "navy")     
+     
+     points(Pageviews ~ Day.Index,  
+            pch = 21,
+            bg = "navy",
+            col = "blue")       
      
      abline(a = 100, 
             b = 0,
