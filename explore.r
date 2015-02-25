6ceb7c3be63517d30359b8cc44b570ca0ac8c763
Tim Hoolihan <tim@hoolihan.net>
2015-02-25 15:51:45 -0500

    Use with block

--- explore.r
+++ explore.r
@@ -1,10 +1,11 @@
 
-# data <- read.csv("~/workspace/data/ga-hoolihan.csv", sep=",")
 data <- read.csv("~/workspace/data/ga2-hoolihan.csv", sep=",")
-data$Day.Index <- as.Date(data$Day.Index, format="%m/%d/%Y")
 
-plot(data$Day.Index, 
-     data$Pageviews,
-     xlab = "Date",
-     type = "b")
-
+with(data, {
+     Day.Index <- as.Date(Day.Index, format="%m/%d/%Y")
+     
+     plot(Day.Index, 
+          Pageviews,
+          xlab = "Date",
+          type = "b")
+})
